79d0068505ccd5f00aa7f8a7686b089709b21d3d
rinatbul <rinatbuldybayev@gmail.com>
2024-06-18 22:00:38 +0500

    Project and Header component created

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+export const Header=()=>{
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
